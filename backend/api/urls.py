52352c59d8f267aa3a182807e592fa0a35df0da4
Xitij Ritesh Patel <ritesh@buildscience.com>
2015-10-29 20:07:57 -0400

    Add trailing slashes to auth urls.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,7 +8,7 @@ router.register(r'accounts', views.AccountViewSet)
 urlpatterns = [
     url(r'v1/', include(router.urls, namespace='v1')),
     url(r'^drf/auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^auth/login', 'rest_framework_jwt.views.obtain_jwt_token'),
-    url(r'^auth/refresh', 'rest_framework_jwt.views.refresh_jwt_token'),
-    url(r'^auth/verify', 'rest_framework_jwt.views.verify_jwt_token'),
+    url(r'^auth/login/$', 'rest_framework_jwt.views.obtain_jwt_token'),
+    url(r'^auth/refresh/$', 'rest_framework_jwt.views.refresh_jwt_token'),
+    url(r'^auth/verify/$', 'rest_framework_jwt.views.verify_jwt_token'),
 ]
